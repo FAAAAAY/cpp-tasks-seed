5a32b2f57b641f2e6e8886edd4c25870b6ea8fe6
Max <faytop1gggg@gmail.com>
2026-06-07 12:48:55 +0300

    First HW

--- 01.InplaceSorting/sort_test.cpp
+++ 01.InplaceSorting/sort_test.cpp
@@ -5,7 +5,56 @@
 
 // Но проверьте, что сортировки таки работают...
 
-TEST(FooSuite, FooTest)
+TEST(BubbleSort, RandomArray)
 {
-    EXPECT_EQ(0, 0);
+    std::vector<int> v = {5, 1, 4, 2, 8};
+
+    bubble_sort(v.begin(), v.end());
+
+    EXPECT_EQ(v, (std::vector<int>{1, 2, 4, 5, 8}));
+}
+
+TEST(BubbleSort, EmptyArray)
+{
+    std::vector<int> v;
+
+    bubble_sort(v.begin(), v.end());
+
+    EXPECT_TRUE(v.empty());
+}
+
+TEST(BubbleSort, OneElement)
+{
+    std::vector<int> v = {42};
+
+    bubble_sort(v.begin(), v.end());
+
+    EXPECT_EQ(v[0], 42);
+}
+
+TEST(QuickSort, RandomArray)
+{
+    std::vector<int> v = {10, 7, 8, 9, 1, 5};
+
+    quick_sort(v.begin(), v.end());
+
+    EXPECT_EQ(v, (std::vector<int>{1, 5, 7, 8, 9, 10}));
+}
+
+TEST(QuickSort, Duplicates)
+{
+    std::vector<int> v = {4, 2, 4, 1, 2};
+
+    quick_sort(v.begin(), v.end());
+
+    EXPECT_EQ(v, (std::vector<int>{1, 2, 2, 4, 4}));
+}
+
+TEST(QuickSort, SortedArray)
+{
+    std::vector<int> v = {1, 2, 3, 4, 5};
+
+    quick_sort(v.begin(), v.end());
+
+    EXPECT_EQ(v, (std::vector<int>{1, 2, 3, 4, 5}));
 }
--- 01.InplaceSorting/sorting.h
+++ 01.InplaceSorting/sorting.h
@@ -5,17 +5,55 @@
 
 // Для swap
 #include "collvalue.h"
+#include <iterator>
 
 // ----------------------------- Пузырёк (например) --------------------------------------
 template <typename Iterator>
 void bubble_sort(Iterator begin, Iterator end)
 {
+    if (begin == end)
+        return;
+
+    bool swapped;
+    Iterator last = end;
+    do {
+        swapped = false;
+        for (Iterator j = begin; std::next(j) != last; ++j)
+        {
+            Iterator next = std::next(j);
+            if (*next < *j)
+            {
+                std::iter_swap(j, next);
+                swapped = true;
+            }
+        }
+        last = std::prev(last); 
+    } while (swapped);
 }
 
 // --------------------------- QuickSort (например) ---------------------------------------
 template <typename Iterator>
 void quick_sort(Iterator begin, Iterator end)
 {
+    if (std::distance(begin, end) <= 1)
+        return;
+
+    Iterator pivot_it = std::prev(end);
+    auto pivot = *pivot_it;
+
+    Iterator middle = begin;
+    for (Iterator it = begin; it != pivot_it; ++it)
+    {
+        if (*it < pivot)
+        {
+            std::iter_swap(it, middle);
+            ++middle;
+        }
+    }
+    std::iter_swap(middle, pivot_it);
+
+    quick_sort(begin, middle);
+    quick_sort(std::next(middle), end);
 }
 
 #endif // SORTING_H
